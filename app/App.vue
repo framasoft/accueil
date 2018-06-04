4d1e89830d482134806dd3d71d709db573c07baf
JosephK <josephk@framasoft.org>
2018-06-04 18:44:18 +0200

    Meta (fix DC.description)

--- app/App.vue
+++ app/App.vue
@@ -6,13 +6,13 @@
       :keywords="$t('msg.meta.keywords')"
       :head="{
         'meta[name=author]': {content:  $t('msg.meta.name') },
-        'meta[name$=title]': {content: $t('msg.meta.title') },
-        'meta[name$=publisher]': {content: $t('msg.meta.name') },
-        'meta[name$=contributor]': {content: $t('msg.meta.name') },
-        'meta[name$=language]': {content: $t('msg.meta.lang') },
-        'meta[name$=subject]': {content: $t('msg.meta.keywords') },
-        'meta[name$=rights]': {content: $t('msg.meta.license') },
-        'meta[name$=description]': {content: $t('msg.meta.desc') },
+        'meta[name^=DC][name$=title]': {content: $t('msg.meta.title') },
+        'meta[name^=DC][name$=publisher]': {content: $t('msg.meta.name') },
+        'meta[name^=DC][name$=contributor]': {content: $t('msg.meta.name') },
+        'meta[name^=DC][name$=language]': {content: $t('msg.meta.lang') },
+        'meta[name^=DC][name$=subject]': {content: $t('msg.meta.keywords') },
+        'meta[name^=DC][name$=rights]': {content: $t('msg.meta.license') },
+        'meta[name^=DC][name$=description]': {content: $t('msg.meta.desc') },
       }"
     />
     <router-view></router-view>
