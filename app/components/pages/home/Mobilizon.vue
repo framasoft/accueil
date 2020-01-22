35b60b11cc93c3a429cf1cf490ce60d46d13184a
JosephK <josephk@framasoft.org>
2020-01-22 15:13:45 +0100

    WIP 👓 cleaning

--- app/components/pages/home/Mobilizon.vue
+++ app/components/pages/home/Mobilizon.vue
@@ -1,35 +1,39 @@
 <template>
-  <section id="mobilizon" class="clearfix">
+  <section
+    id="mobilizon"
+    class="clearfix"
+  >
     <div class="embed-responsive embed-responsive-16by9">
-      <img :src="`${$t('/')}img/mobilizon/event.gif`" alt="" style="width: 100%" />
+      <img
+        :src="`${$t('/')}img/mobilizon/event.gif`"
+        alt=""
+        style="width: 100%"
+      />
     </div>
     <div class="container ombre">
       <h2 v-html="$t('home.mobilizon.title')"></h2>
       <p v-html="$t('home.mobilizon.text')"></p>
       <p>
-        <a :href="$t('home.mobilizon.link')"
+        <a
+          :href="$t('home.mobilizon.link')"
           class="btn btn-default"
-          v-html="$t('home.mobilizon.btn')">
-        </a>
+          v-html="$t('home.mobilizon.btn')"
+        ></a>
       </p>
     </div>
-    <p class="pfooter" v-html="$t('home.mobilizon.footer')"></p>
+    <p
+      class="pfooter"
+      v-html="$t('home.mobilizon.footer')"
+    ></p>
   </section>
 </template>
 
 <script>
-import { Modal, Tabs, Tab} from 'uiv';
-
 export default {
-  components: {
-    Modal,
-    Tabs,
-    Tab,
-  },
   data() {
     return {
       videoBackground: 'about:blank',
-    }
+    };
   },
-}
+};
 </script>
